c91c8da228ddd14b6acc1124e9c874f6ecf994d4
Shivom <shivomthakur38@gmail.com>
2022-10-02 14:52:46 +0530

    pushed-some-code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+/*library imports*/
 import React from "react";
 import ReactDOM from "react-dom";
 import styled from "styled-components";
-
+/*custom imports*/
 import WeekCalendar from "./components/WeekSchedular";
 
+/*styled-components*/
 const AppContainer = styled.div`
-background-color:#FBFCFC
-`
+  background-color: #fbfcfc;
+`;
+
 const App = () => {
   return (
     <AppContainer>
